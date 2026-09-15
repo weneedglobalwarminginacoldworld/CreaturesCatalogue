01fc694ef66c9b302351e332c0ca299fb1b33ab0
blickyMinaj <c4explosiveboom@gmail.com>
2026-09-15 16:24:59 +0300

    finally figured the structure

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,51 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from typing import Any, Literal, Tuple
+'''
+This is the first iteration of the concept. 
+I will use path parameters to receive edit
+proposals from a client and return the relevant changes
+'''
 
-app = FastAPI()
+import msgspec
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, JSONResponse
+import markupsafe
+import sqlite3
+import string
 
-class Properties(BaseModel):
-    size: Tuple[int, int] = (10, 10) #px
-    link_to: Any | None = None
-    bg_color: str = 'white'
-    img: bool = False
-    text: bool = False
-    text_content: str | None = None
-    text_font: str|None = None
+from starlette.routing import Route
 
-def insert_at(html: str, content: str):
-    count = 0
-    opening_tag: str = ''
-    closing_tag: str = ''
-    for letter in html:
-        count += 1
-        if letter == '>':
-            opening_tag = html[:count-1]
-            closing_tag = html[count-1:]
-            count = 0
-    return opening_tag + content + closing_tag
+user_file = {}
 
-@app.post('/edit/{page}')
-def make_changes(
-        page: str,
-        properties: Properties
-        ):
-    html: str = ''
-    if properties.link_to is not None:
-        html = '<a></a>'
-    if properties.img:
-        #needs a helper function to check what html currently looks like and decid where to insert 
-        #i have written it
-        insert_at(html, '<img></img>')
-    #etc
-    return html
+class Properties(msgspec.Struct):
+    bg_color: str = 'red'
+
+decoder = msgspec.json.Decoder(type=Properties)
+
+def main_grid(request):
+    html = '''<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <h1>Hello There</h1>
+  </body>
+</html>'''
+    return HTMLResponse(html)
+
+async def pick_bg(request: Request):
+    r_bytes = await request.body()
+    properties = decoder.decode(r_bytes)
+    color = properties.bg_color
+    user_file['bg_color'] = color
+    #atp i should somehow make the html elem have a style tag
+    #but for now i'll just return a whole thing
+    return HTMLResponse(f'''<!DOCTYPE html>
+<html style='background-color: {color}'>
+  <head></head>
+  <body>
+    <h1>Hello There</h1>
+  </body>
+</html>''')
+
+app = Starlette(routes=[Route('/', main_grid),
+                        Route('/pick_bg', pick_bg, methods=['POST'])],)
+
